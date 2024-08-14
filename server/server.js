f8b4369af3321db717042d01d2e3315eed9fbc14
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2024-08-14 14:45:22 +1200

    Add .env file

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import * as dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes.js';
 import itemRoutes from './routes/itemRoutes.js';
 
