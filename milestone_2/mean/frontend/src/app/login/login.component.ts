893ff3a8398d259807e6663190e05500e584a0fa
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2024-09-19 16:44:02 +1200

    Fix profile display

--- milestone_2/mean/frontend/src/app/login/login.component.ts
+++ milestone_2/mean/frontend/src/app/login/login.component.ts
@@ -19,14 +19,14 @@ export class LoginComponent implements OnInit {
   isLoggedIn = false;
   isLoginFailed = false;
   errorMessage = '';
-  roles: string[] = [];
+  role: string = '';
 
   constructor(private authService: AuthService, private storageService: StorageService) { }
 
   ngOnInit(): void {
     if (this.storageService.isLoggedIn()) {
       this.isLoggedIn = true;
-      this.roles = this.storageService.getUser().roles;
+      this.role = this.storageService.getUser().role;
     }
   }
 
@@ -39,7 +39,7 @@ export class LoginComponent implements OnInit {
 
         this.isLoginFailed = false;
         this.isLoggedIn = true;
-        this.roles = this.storageService.getUser().roles;
+        this.role = this.storageService.getUser().role;
         this.reloadPage();
       },
       error: err => {
