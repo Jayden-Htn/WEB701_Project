5e19c6729014529aee3066cf6bbfcc98a74f6ed4
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2024-11-13 14:17:12 +1300

    Update setup

--- milestone_3/backend/setup.js
+++ milestone_3/backend/setup.js
@@ -14,11 +14,13 @@ async function initial() {
             const beneficiaryRole = await new Role({ name: "beneficiary" }).save();
             const staffRole = await new Role({ name: "staff" }).save();
             const adminRole = await new Role({ name: "admin" }).save();
+            const donatorRole = await new Role({ name: "donator" }).save();
             console.log("Added roles to roles collection");
 
             roleIds.beneficiary = beneficiaryRole._id;
             roleIds.staff = staffRole._id;
             roleIds.admin = adminRole._id;
+            roleIds.donator = donatorRole._id;
         }
         
         count = await Category.estimatedDocumentCount();
@@ -34,9 +36,9 @@ async function initial() {
     
         count = await Item.estimatedDocumentCount();
         if (count === 0) {
-            const phoneItem = await new Item({ name: "Phone", description: "Description", price: 60, quantityAvailable: 3, category: "66e7b1460d0a09120693b274" }).save();
-            await new Item({ name: "Computer", description: "Description", price: 60, quantityAvailable: 3, category: "66e7b1460d0a09120693b276" }).save();
-            await new Item({ name: "Tablet", description: "Description", price: 60, quantityAvailable: 3, category: "66e7b1460d0a09120693b278" }).save();
+            const phoneItem = await new Item({ name: "Phone", description: "Description", price: 60, quantityAvailable: 3, category: "66f74e4c2f8b243641d205b8" }).save();
+            await new Item({ name: "Computer", description: "Description", price: 60, quantityAvailable: 3, category: "66f74e4c2f8b243641d205ba" }).save();
+            await new Item({ name: "Tablet", description: "Description", price: 60, quantityAvailable: 3, category: "66f74e4c2f8b243641d205bc" }).save();
             console.log("Added example items to items collection");
 
             itemIds.phone = phoneItem._id;
@@ -48,9 +50,11 @@ async function initial() {
                 organisation: "", tokens: 200, tokenExpiry: new Date("2024-09-15T22:06:39.000+00:00"), role: roleIds.beneficiary, purchases: [itemIds.phone, itemIds.phone] }).save();
             await new User({ firstName: "Steve", lastName: "Smith", email: "staff@gmail.com", password: "$2a$08$LgXPtEHjf/XBRQs0xbZV0ez3znS0JzDYkux2uSsrEx0jbkv27F.4C", 
                 organisation: "", tokens: 0, tokenExpiry: new Date("2024-09-15T22:06:39.000+00:00"), role: roleIds.staff, purchases: [] }).save();
-            console.log("Added user accounts to user collection");
             await new User({ firstName: "Adam", lastName: "Smith", email: "admin@gmail.com", password: "$2a$08$dunM.GAMVu7Jtcm31zVpvu/XdKDpBH4kHdzwSvG0OtZZMoXN0Wx6u", 
                 organisation: "", tokens: 0, tokenExpiry: new Date("2024-09-15T22:06:39.000+00:00"), role: roleIds.admin, purchases: [] }).save();
+            await new User({ firstName: "Dave", lastName: "Smith", email: "donator@gmail.com", password: "$2a$08$4pgZ.70Z2NPW369JvSccCORCAQVkCAEOOuf0ImgFwAC4Z9djefVky", 
+                organisation: "", tokens: 0, tokenExpiry: new Date("2024-09-15T22:06:39.000+00:00"), role: roleIds.donator, purchases: [] }).save();
+            console.log("Added user accounts to user collection");
         }
     } catch (err) {
       console.error("Error during initial setup:", err);
